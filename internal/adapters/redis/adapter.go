d7e6cca412c7f3fcbc8bdb3ec3d8a15d86930836
Igor Grieder Melo <igorgrieder@gmail.com>
2025-08-13 08:52:34 -0300

    internal added

--- internal/adapters/redis/adapter.go
+++ internal/adapters/redis/adapter.go
@@ -14,32 +14,32 @@ type Options struct {
 	Protocol int
 }
 
-type Adapter struct {
+type Redis struct {
 	client *goredis.Client
 }
 
-func NewAdapter(opts Options) *Adapter {
+func NewAdapter(opts Options) *Redis {
 	client := goredis.NewClient(&goredis.Options{
 		Addr:     opts.Addr,
 		Password: opts.Password,
 		DB:       opts.DB,
 		Protocol: opts.Protocol,
 	})
-	return &Adapter{client: client}
+	return &Redis{client: client}
 }
 
-func (a *Adapter) Ping(ctx context.Context) error {
+func (a *Redis) Ping(ctx context.Context) error {
 	return a.client.Ping(ctx).Err()
 }
 
-func (a *Adapter) Close() error {
+func (a *Redis) Close() error {
 	return a.client.Close()
 }
 
-func (a *Adapter) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
+func (a *Redis) Set(ctx context.Context, key string, value string, ttl time.Duration) error {
 	return a.client.Set(ctx, key, value, ttl).Err()
 }
 
-func (a *Adapter) Get(ctx context.Context, key string) (string, error) {
+func (a *Redis) Get(ctx context.Context, key string) (string, error) {
 	return a.client.Get(ctx, key).Result()
 }
