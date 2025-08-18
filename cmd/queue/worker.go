cc423b0c79e33d6f1c80ac84b048b149743c1e9a
Igor Grieder Melo <igorgrieder@gmail.com>
2025-08-17 21:35:55 -0300

    worker settings

--- cmd/queue/worker.go
+++ cmd/queue/worker.go
@@ -1,7 +1,38 @@
 package queue
 
-import "github.com/redis/go-redis/v9"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+)
+
+type paymentInput struct {
+	correlationId uuid.UUID
+	amount        float32
+	requestedAt   string
+}
 
 func StartPaymentQueue(redis *redis.Client) {
+	ctx := context.Background()
+	queue := "payment-queue"
 
+	for {
+		data, err := redis.BLPop(ctx, 0, queue).Result()
+		if err != nil {
+			return
+		}
+
+		json, err := json.Marshal(data[1])
+		req, err := http.NewRequest("POST", "http://payment-processor-default:8080", bytes.NewBuffer(json))
+		if err != nil {
+			return
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+
+	}
 }
